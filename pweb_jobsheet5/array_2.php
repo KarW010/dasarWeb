3ea44194849afaf50d934ec1d77478f46769515a
KarW010 <kartikostudy@gmail.com>
2025-09-26 09:44:19 +0700

    Jobsheet 5

---
+++ pweb_jobsheet5/array_2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Data Dosen</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 8px;
+        }
+    </style>
+</head>
+<body>
+<?php
+    $Dosen = [
+        'nama' => 'Elok Nur Hamdana',
+        'domisili' => 'Malang',
+        'jenis_kelamin' => 'Perempuan'
+    ];
+?>
+
+<table>
+    <tr>
+        <th>Nama</th>
+        <td><?php echo $Dosen['nama']; ?></td>
+    </tr>
+    <tr>
+        <th>Domisili</th>
+        <td><?php echo $Dosen['domisili']; ?></td>
+    </tr>
+    <tr>
+        <th>Jenis Kelamin</th>
+        <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+    </tr>
+</table>
+
+</body>
+</html>
